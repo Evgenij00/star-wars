a4a1bad314d73013b6654d4553ea859a61221cb5
Evgeny <e.ganeev@outlines.tech>
2022-03-22 23:41:51 +0300

    add utils

---
+++ src/utils/transform.js
@@ -0,0 +1,33 @@
+export const transformPlanet = planet => {
+  return {
+    id: this._extractId(planet),
+    name: planet.name,
+    population: planet.population,
+    rotationPeriod: planet.rotation_period,
+    diameter: planet.diameter,
+  }
+}
+
+export const transformStarship = starship => {
+  return {
+    id: this._extractId(starship),
+    name: starship.name,
+    model: starship.model,
+    manufacturer: starship.manufacturer,
+    costInCredits: starship.cost_in_credits,
+    length: starship.length,
+    crew: starship.crew,
+    passengers: starship.passengers,
+    cargoCapacity: starship.cargo_capacity,
+  }
+}
+
+export const transformPerson = person => {
+  return {
+    id: this._extractId(person),
+    name: person.name,
+    gender: person.gender,
+    birthYear: person.birth_year,
+    eyeColor: person.eye_color,
+  }
+}
